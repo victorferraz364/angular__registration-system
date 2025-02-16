2d6f067bb39ecc20ace32077d491af621ddbb7e3
unknown <victorferraz364@gmail.com>
2025-02-16 14:02:51 -0300

    :heavy_plus_sign: [build] add angular material to project

--- src/app/freatures/nutrition.module/nutrition.module.ts
+++ src/app/freatures/nutrition.module/nutrition.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { NutritionPageComponent } from './pages/nutrition.page/nutrition.page.component';
+import { AngularMaterialModule } from "src/app/shared/angular-material/angular-material.module";
 
 @NgModule({
-    imports:[],
+    imports:[
+      AngularMaterialModule
+    ],
     exports:[],
     declarations:[
     NutritionPageComponent
---
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { MatDividerModule } from '@angular/material/divider';
+
+  @NgModule({
+    imports: [
+      MatDividerModule
+    ],
+    exports: [
+      MatDividerModule
+    ],
+
+  })
+  export class AngularMaterialModule {}
+
+  
