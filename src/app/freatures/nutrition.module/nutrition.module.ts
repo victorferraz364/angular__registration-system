d247593298f91a866c47f3a7d0f5dd4ab2e8e740
unknown <victorferraz364@gmail.com>
2025-02-16 13:48:49 -0300

    :construction: nutrition page

--- src/app/freatures/nutrition.module/nutrition.module.ts
+++ src/app/freatures/nutrition.module/nutrition.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
+import { NutritionPageComponent } from './pages/nutrition.page/nutrition.page.component';
 
 @NgModule({
     imports:[],
     exports:[],
-    declarations:[]
+    declarations:[
+    NutritionPageComponent
+  ]
 })
 export class nutritionModule {
     
---
+++ src/app/freatures/nutrition.module/pages/nutrition.page/nutrition.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nutrition.page',
+  templateUrl: './nutrition.page.component.html',
+  styleUrls: ['./nutrition.page.component.scss']
+})
+export class NutritionPageComponent {
+
+}
